76964211435335ab2ae6c3db7dfd82dbf44ad1c2
wpierson <wpierson@lexmark.com>
2012-07-24 09:19:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Micro::Application.config.secret_token = 'd4bcf9b3739c1ddcba58a557f43ce927560192ef009a865dd9bc75f203a87f6bbc4ddfbbfdf499e7b67b185c8abff37322403739c92fa858b4ed3ef5db22f430'
