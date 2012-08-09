1fec21e3c7bcaced797b9fb2177ecb1ff2ed5473
wpierson <wpierson@lexmark.com>
2012-08-09 13:29:07 -0400

    Finished microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
-  before_filter :signed_in_user, only: [:home]
-
   def home    
+    if signed_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
@@ -9,11 +11,4 @@ class StaticPagesController < ApplicationController
 
   def about
   end
-
-  private
-    def signed_in_user
-      if signed_in?
-        redirect_to current_user
-      end
-    end
 end
